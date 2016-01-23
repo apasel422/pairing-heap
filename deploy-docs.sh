79695208b9325e674104686f00ddedd475eb7304
Andrew Paseltiner <apaseltiner@gmail.com>
2016-01-22 19:56:17 -0500

    Add `deploy-docs.sh`

---
+++ deploy-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+cd target/doc
+
+git init
+git config user.email 'apaseltiner@gmail.com'
+git config user.name 'Andrew Paseltiner'
+git remote add upstream "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+git fetch upstream gh-pages
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Rebuild pages at ${TRAVIS_TAG}"
+git push -q upstream HEAD:gh-pages
